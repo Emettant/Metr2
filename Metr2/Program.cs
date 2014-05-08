5c37a8e619b8dbdf5f8eb5a9a375d68a3110049e
Emett <vit.for.reg@gmail.com>
2014-05-08 16:49:54 +0300

    XML Serialization. Adapter to Mathlink started

--- Metr2/Program.cs
+++ Metr2/Program.cs
@@ -71,33 +71,55 @@ using System.Diagnostics;
 
 using System.Collections.Generic;
 
+
 namespace MetrExamples
 {
-    class D {
-        public void d_op1() {
-            B.PublicMethod1_B();
-           // B.PublicMethod2_B();
-            C.PublicMethod1_C();
-        }
+    class PropertyClass {
 
+        int d_field1;
         public int d_pro1 {
             get {
                 B.PublicMethod2_B();//*** for CBO is the same that lower// that's why counted 1 per d_pro1
-                return 0;
+                return d_field1;
             }
             set {
                 C.PublicMethod1_C();
                 C.PublicMethod2_B();//*** for CBO counted as method B.PublicMethod2_B(), which was present earlier // that's why counted 1 per d_pro1
+                d_field1 = value;
             }
+        }
+    }
+
+    //CBO == 2, PublicMethod1_B() called in different places
+    class DiffPlaceSameExternalMethodCall {
+        public void op1() {
+            B.PublicMethod1_B();
+        }
+        public void op2()
+        {
+            B.PublicMethod1_B();
+        }
+    }
+
+    //CBO == 0, no count for self calling
+    class SingleClass {
+        public void s_op1() {
+            s_op2();
+        }
+        public void s_op2()
+        {
+
         }
     }
 
     class E
     {
+        //CBO == 2, because B.PublicMethod1_B() and C.PublicMethod1_B() are the same
         public void e_op1()
         {
             B.PublicMethod1_B();
-            // B.PublicMethod2_B();
+            C.PublicMethod1_B();
+
             C.PublicMethod1_C();
         }
     }
@@ -274,13 +296,23 @@ namespace MetrTest {
             try
             {
                 Microsoft.VisualStudio.TestTools.UnitTesting.
-                Assert.AreEqual(4,
-                Metr.MetricCalculator.CBO(_solution, _compilation, ExampleNamespaceName + ".D"), "Blah");
-
-                Microsoft.VisualStudio.TestTools.UnitTesting.
                 Assert.AreEqual(2,
                 Metr.MetricCalculator.CBO(_solution, _compilation, ExampleNamespaceName + ".E"), "Blah");
 
+                Microsoft.VisualStudio.TestTools.UnitTesting.
+                Assert.AreEqual(0,
+                Metr.MetricCalculator.CBO(_solution, _compilation, ExampleNamespaceName + ".SingleClass"), "Blah");
+
+                Microsoft.VisualStudio.TestTools.UnitTesting.
+                Assert.AreEqual(0,
+                Metr.MetricCalculator.CBO(_solution, _compilation, ExampleNamespaceName + ".DiffPlaceSameExternalMethodCall"), "Blah");
+
+                
+
+                Microsoft.VisualStudio.TestTools.UnitTesting.
+                Assert.AreEqual(5,
+                Metr.MetricCalculator.CBO(_solution, _compilation, ExampleNamespaceName + ".D"), "Blah");
+               
                 Microsoft.VisualStudio.TestTools.UnitTesting.
                 Assert.AreEqual(0,
                 Metr.MetricCalculator.CBO(_solution, _compilation, ExampleNamespaceName + ".A"), "Blah");
@@ -302,7 +334,6 @@ namespace MetrTest {
 
 }
 
-
 namespace Metr
 {
     class MetricCalculatorCore  {
@@ -322,12 +353,12 @@ namespace Metr
         /// it could be Method or Property 
         /// 
         /// </summary>
-        static private Dictionary<Compilation, Dictionary<ISymbol, List<ISymbol>>> _cacheMethodsCouplingField;
-        static protected Dictionary<Compilation, Dictionary<ISymbol, List<ISymbol>>> _cacheMethodsCoupling
+        static private Dictionary<Compilation, Dictionary<ISymbol, HashSet<ISymbol>>> _cacheMethodsCouplingField;
+        static protected Dictionary<Compilation, Dictionary<ISymbol, HashSet<ISymbol>>> _cacheMethodsCoupling
         {
             get
             {
-                if (_cacheMethodsCouplingField == null) _cacheMethodsCouplingField = new Dictionary<Compilation, Dictionary<ISymbol, List<ISymbol>>>();
+                if (_cacheMethodsCouplingField == null) _cacheMethodsCouplingField = new Dictionary<Compilation, Dictionary<ISymbol, HashSet<ISymbol>>>();
                 return _cacheMethodsCouplingField;
             }
         }
@@ -335,7 +366,7 @@ namespace Metr
         static protected void BuildCompilationCacheMap(Solution solution, Compilation compilation)
         {
             _cacheTypesHierarchy.Add(compilation, new Dictionary<ITypeSymbol, List<ITypeSymbol>>());
-            _cacheMethodsCoupling.Add(compilation, new Dictionary<ISymbol, List<ISymbol>>());
+            _cacheMethodsCoupling.Add(compilation, new Dictionary<ISymbol, HashSet<ISymbol>>());
 
             //for every SyntaxTree was analysed in current compilation
             //for every "global-declared" namespace member was present in this compilation
@@ -368,20 +399,40 @@ namespace Metr
                     dfs(solution, compilation, to);
                 }
 
-                ISymbol meth = cur as IMethodSymbol;
-                if (meth == null) meth = cur as IPropertySymbol;
-                if (meth != null)
-                {
-                    var too = SymbolFinder.FindCallersAsync(meth, solution);
-                    too.Wait();
-                    foreach (var el in too.Result)
+                var asMeth = cur as IMethodSymbol;
+                var asPro = cur as IPropertySymbol;
+                var MethodList = new List<IMethodSymbol> { asMeth };
+                //we do not need analyse property, because its methods already is members to see
+                //if (asPro != null) MethodList = new List<IMethodSymbol> { asPro.GetMethod, asPro.SetMethod};
+
+                foreach (var meth in MethodList) {
+                    if (meth != null)
                     {
-                        if (!_cacheMethodsCoupling[compilation].ContainsKey(el.CallingSymbol))
-                            _cacheMethodsCoupling[compilation].Add(el.CallingSymbol, new List<ISymbol>());
+                        var too = SymbolFinder.FindCallersAsync(meth, solution);
+                        too.Wait();
+                        var CallingMethods = new List<IMethodSymbol>();
+                        foreach (var el in too.Result) {
+                            var method = el.CallingSymbol as IMethodSymbol;
+                            if (method != null) CallingMethods.Add(method);
+                            var property = el.CallingSymbol as IPropertySymbol;
+                            if (property != null)
+                            {
+                                CallingMethods.Add(property.GetMethod); CallingMethods.Add(property.SetMethod);
+                            }
+                        }
+                   
+                        foreach (var el in CallingMethods)
+                        {
+                            if (el.ContainingType != meth.ContainingType)
+                            {
+                                if (!_cacheMethodsCoupling[compilation].ContainsKey(el))
+                                    _cacheMethodsCoupling[compilation].Add(el, new HashSet<ISymbol>());
 
-                        _cacheMethodsCoupling[compilation][el.CallingSymbol].Add(meth);
+                                _cacheMethodsCoupling[compilation][el].Add(meth);
+                            }
+                        }
+
                     }
-
                 }
             }
         }
@@ -446,25 +497,42 @@ namespace Metr
             Int32 ret = 0;
             foreach (ISymbol mem in cur.GetMembers()) {
                 ISymbol meth = mem as IMethodSymbol;
-                if (meth == null) meth = mem as IPropertySymbol;
+                // There are property.get ans property.set concidered as IMethodSymbol-s, 
+                //if (meth == null) meth = mem as IPropertySymbol;
                 if (meth == null) continue;
+
                 if (_cacheMethodsCoupling[compilation].ContainsKey(meth))
-                    ret += _cacheMethodsCoupling[compilation][meth].Count();    
+                {
+                    var temp = _cacheMethodsCoupling[compilation][meth];
+                    ret += _cacheMethodsCoupling[compilation][meth].Count();
+                }
+                
             }
             return ret;
         }
+
+        static public Int32 RFC(Solution solution, Compilation compilation, String className) {
+            return 0;//CBO(Solution solution, Compilation compilation, String className) 
+            }
 
 
     }
 
 
+  
+   
+}
+
+namespace MetrMain {
     class Program
     {
         static void Main(string[] args)
         {
-            MetrTest.MetricCalculatorTest.Run();
+            //MetrTest.MetricCalculatorTest.Run();
+            // MetrExpertXML.EstimationListTest.Run();
+            MetrMath.ModelTest.Run();
 
-            
+
 
             //string solutionPath = @"C:\Users\Vitaliy\Documents\Visual Studio 2013\Projects\Metr2\Metr2.sln";
             //var workspace = MSBuildWorkspace.Create();
@@ -472,7 +540,7 @@ namespace Metr
             //var project = solution.Projects.Where(p => p.Name == "Metr2").First();
             //var compilation = project.GetCompilationAsync().Result;
 
-            
+
             //var programClass = compilation.GetTypeByMetadataName("RoslynTest.Program");
 
             //var barMethod = programClass.GetMembers("Bar").First();
@@ -485,17 +553,137 @@ namespace Metr
             //Debug.Assert(fooResult.First().Locations.Count() == 0);
         }
 
-        public bool Foo()
-        {
-            return "Bar" == Bar();
-        }
+        //public bool Foo()
+        //{
+        //    return "Bar" == Bar();
+        //}
 
-        public string Bar()
-        {
-            return "Bar";
+        //public string Bar()
+        //{
+        //    return "Bar";
+        //}
+    }
+}
+
+namespace MetrExpertXML {
+    using System.Xml.Serialization;
+    using System.IO;
+
+    [XmlType("EstimationOfElement")]
+    public class EstimationOfElement{
+        [XmlAttribute("Solution")]
+        public string Solution { get; set; }
+
+        [XmlAttribute("Project")]
+        public string Project { get; set; }
+
+        [XmlAttribute("FullName")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("Estimation")]
+        public int Estimation { get; set; }
+
+        public EstimationOfElement() { }
+        public EstimationOfElement(string solution, string project, string name, int est) {
+            this.Solution = solution;
+            this.Project = project;
+            this.FullName = name;
+            this.Estimation = est;
         }
     }
 
-   
+    [XmlRoot("EstimationList")]
+    [XmlInclude(typeof(EstimationOfElement))] // include type class EstimationOfElement
+    public class EstimationList
+    {
+        [XmlArray("EstimationArray")]
+        [XmlArrayItem("EstimationObject")]
+        public List<EstimationOfElement> Estimations = new List<EstimationOfElement>();
+
+        [XmlElement("ListName")]
+        public string ListName { get; set; }
+
+        public EstimationList() { }
+
+        public EstimationList(string name)
+        {
+            this.ListName = name;
+        }
+
+        public void AddEstimation(EstimationOfElement estimation)
+        {
+            Estimations.Add(estimation);
+        }
+    }
+
+    public class EstimationListTest {
+        static public void Run() {
+
+            var estimList = new EstimationList();
+            estimList.ListName = "TestEstims";
+
+            estimList.AddEstimation(new EstimationOfElement("Metr2","Metr2", "MetrExamples.D",33));
+            estimList.AddEstimation(new EstimationOfElement("Metr2", "Metr2", "MetrExamples.B", 11));
+            estimList.AddEstimation(new EstimationOfElement("Metr2", "Metr2", "MetrExamples.A", 12));
+
+            // Serialize 
+            Type[] estimationTypes = { typeof(EstimationOfElement) };
+            XmlSerializer serializer = new XmlSerializer(typeof(EstimationList), estimationTypes);
+            FileStream fs = new FileStream("EstimationList.xml", FileMode.Create);
+            serializer.Serialize(fs, estimList);
+            fs.Close();
+            estimList = null;
+
+            // Deserialize 
+            fs = new FileStream("EstimationList.xml", FileMode.Open);
+            estimList = (EstimationList)serializer.Deserialize(fs);
+            serializer.Serialize(Console.Out, estimList);
+        }
+    }
+
 }
 
+namespace MetrMath {
+    using Wolfram.NETLink;
+    using Adapter;
+
+    static class Adapter {
+        static IKernelLink kl = null;
+        public static string Calc(string input) {
+            if (kl == null) {
+                kl = MathLinkFactory.CreateKernelLink();//"-linkmode launch -linkname 'c:\\program files\\wolfram research\\mathematica\\9.0\\mathkernel'");
+                kl.WaitAndDiscardAnswer();
+            }
+            return kl.EvaluateToOutputForm(input, input.Length);
+        }
+        public static string ListToString<T>(List<T> list){
+            var sb = new System.Text.StringBuilder("{");
+            var separator = ", ";
+            foreach (var el in list) {
+                sb.Append(el.ToString());
+                sb.Append(separator);
+            }
+            sb.Remove(sb.Length - separator.Length, separator.Length);
+            sb.Append("}");
+            return sb.ToString();
+        }
+    }
+
+    class Model {
+        static public void Build() {
+
+            
+        }
+    }
+
+    class ModelTest {
+        
+        static public void Run() {
+            //Model.Build();
+            Console.WriteLine(ListToString<Int32>(new List<Int32> { 2, 3, 4 }));
+            Console.WriteLine(Calc(ListToString<Int32>(new List<Int32> { 2, 3, 4 })+"+"+ ListToString<Int32>(new List<Int32> { 2, 3, 4 })));
+        }
+    }
+
+
+}
